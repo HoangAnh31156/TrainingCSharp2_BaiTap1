5e0ca63789958a49b5cc19f9e5c76d5a251ead03
Vanh2110 <hoangvanh2110@gmail.com>
2023-09-19 10:02:13 +0700

    Add project files.

---
+++ TrainingCSharp2_BaiTap1/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingCSharp2_BaiTap1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.InputEncoding = Encoding.UTF8;
+
+
+            MENU();
+        }
+        static void MENU()
+        {
+            SERVICE service = new SERVICE();
+            List<Bike> _lstBikes = new List<Bike>
+            {
+                new Bike(123, "GN 2.0 27", "Thống nhất"),
+                new Bike(14, "MTP 2.1", "Thống nhất"),
+                new Bike(625, "The flash", "MoveHiHo"),
+                new Bike(226, "SLX - TB", "HaHaMove"),
+
+            };
+
+            string path = @"E:\CODE\VisualStudio\TrainingCSharp2_BaiTap1\TrainingCSharp2_BaiTap1\FileBike.txt";
+            File.WriteAllText(path, "");
+
+            int chon;
+            do
+            {
+                Console.WriteLine("------------MENU-------------");
+                Console.WriteLine("1. Nhập DS (Mã ID tự tăng khi nhập vào)");
+                Console.WriteLine("2. Xuất DS");
+                Console.WriteLine("3. Ghi file");
+                Console.WriteLine("4. Đọc file");
+                Console.WriteLine("5. Xóa đối tượng theo mã ID ");
+                Console.WriteLine("0. Thoát CT");
+                Console.WriteLine("-----------------------------");
+                Console.Write("Mời bạn chọn chức năng:  ");
+                chon = Convert.ToInt32(Console.ReadLine());
+                switch (chon)
+                {
+                    case 1:
+                        service.NhapDS(_lstBikes);
+                        break;
+                    case 2:
+                        service.XuatDS(_lstBikes);
+                        break;
+                    case 3:
+                        service.GhiFile(path, _lstBikes);
+                        break;
+                    case 4:
+                        List<Bike> bike = service.DocFile(path);
+                        foreach (var item in _lstBikes)
+                        {
+                            item.InThongTin();
+                        }
+                        break;
+                    case 5:
+                        service.Xoa(_lstBikes);
+                        break;
+                    case 0:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Moi chon lai");
+                        break;
+                }
+
+            } while (chon != 0);
+        }
+
+    }
+}
---
+++ TrainingCSharp2_BaiTap1/SERVICE.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace TrainingCSharp2_BaiTap1
+{
+    internal class SERVICE
+    {
+        public void NhapDS(List<Bike> _lstBikes)
+        {
+            int id = 1;
+            string tiepTuc;
+            do
+            {              
+                Bike bike = new Bike();
+                while (_lstBikes.Any(x => x.ID1 == id))
+                {
+                    id++;
+                }
+                bike.ID1 = id;
+                Console.Write("Nhập tên:  ");
+                bike.Ten1 = Console.ReadLine();
+                Console.Write("Nhập hãng sản xuất:  ");
+                bike.HSX1 = Console.ReadLine();
+                _lstBikes.Add(bike);
+                Console.WriteLine("Ban co muon tiep tuc ko ? (c/k)");
+                tiepTuc = Console.ReadLine();
+            } while (tiepTuc.Equals("co"));
+        }
+        
+        public void XuatDS(List<Bike> _lstBikes)
+        {
+            if(_lstBikes.Count == 0)
+            {
+                Console.WriteLine("Chưa có thông tin !");
+                return;
+            }
+            foreach (var item in _lstBikes)
+            {
+                item.InThongTin();
+            }
+        }
+        public void GhiFile(string path, List<Bike> _lstBikes)
+        {
+            if(File.Exists(path))
+            {
+                foreach (var item in _lstBikes)
+                {
+                    string line = item.ObjToStr();
+                    File.AppendAllText(path, line);
+                }
+                Console.WriteLine("Ghi file thanh cong !");
+            }
+            else
+            {
+                Console.WriteLine("File Khong ton tai !");
+            }
+        }
+        public List<Bike>  DocFile(string path)
+        {
+            List<Bike> _lstBikes = new List<Bike>();
+            string[] lines = File.ReadAllLines(path);
+            foreach (string line in lines)
+            {
+                if(line.Trim().Length == 0)
+                {
+                    continue;
+                }
+                else
+                {
+                    string[] attributes = line.Split('-');
+                    Bike bike = new Bike();
+                    bike.ID1 = Convert.ToInt32(attributes[0]);
+                    bike.Ten1 = attributes[1];
+                    bike.HSX1 = attributes[2];
+                    _lstBikes.Add(bike);
+                }
+            }
+            return _lstBikes;
+        }
+        public void Xoa(List<Bike> lstBikes)
+        {
+            Console.Write("Nhập ID cần xóa :  ");
+            int id = Convert.ToInt32(Console.ReadLine());
+            var XoaQuery =
+                (from b in lstBikes
+                 where b.ID1 == id
+                 select b).FirstOrDefault();
+            if(XoaQuery == null)        
+            {
+                Console.WriteLine("Không tìm thấy ID cần tìm !");
+                return;
+            }
+            else
+            {
+                lstBikes.Remove(XoaQuery);
+            }
+        }
+        
+    }
+}
